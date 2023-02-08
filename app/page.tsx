ce4b66556d278b3ecd0f3448c3e0b5995982ddb1
JD-007 <jaan.abbott99@gmail.com>
2023-02-08 17:49:49 +0500

    Blog - 8th Feb

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,19 @@ import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 import Link from 'next/link'
 
+import { posts } from '../data/posts'
+import { StringLiteral } from 'typescript'
+
+
 
 const inter = Inter({ subsets: ['latin'] })
+
+type Post = {
+
+  id: string,
+  title: string,
+  date: string,
+}
 
 export default function Home() {
   return (
@@ -13,6 +24,20 @@ export default function Home() {
 
       <div className={styles.center}>
         <h1> HOME </h1>
+      </div>
+
+      <div>
+      { posts.map(({id, title, date}: Post) => (
+        <>
+        <Link href="/posts"> {title}</Link>
+        <br />
+        <p> {date}</p>
+        <br />
+        
+        </>
+
+      )) }
+
       </div>
 
       <Link href="/posts"> To Posts </Link>
